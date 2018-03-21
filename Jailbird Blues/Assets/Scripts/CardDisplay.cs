1120166d38a2207b2e8e4565679b681510c1f7e8
Raqdoll <ilarisalonen112@gmail.com>
2018-03-21 13:56:08 +0200

    Card basics

---
+++ Jailbird Blues/Assets/Scripts/CardDisplay.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CardDisplay : MonoBehaviour {
+
+    public CardValues card;
+
+    public Text cardText;
+    public Text button1;
+    public Text button2;
+    public Text button3;
+    public Text button4;
+
+    void Start () {
+
+        cardText.text = card.cardText;
+        button1.text = card.option1;
+        button2.text = card.option2;
+        button3.text = card.option3;
+        button4.text = card.option4;
+
+    }
+	
+}
