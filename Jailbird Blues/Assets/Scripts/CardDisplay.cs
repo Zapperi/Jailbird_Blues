65d2b24da3f81c96e7a47fc61ba1f70ba3083632
Raqdoll <ilarisalonen112@gmail.com>
2018-03-22 12:27:18 +0200

    To iltapäivä

--- Jailbird Blues/Assets/Scripts/CardDisplay.cs
+++ Jailbird Blues/Assets/Scripts/CardDisplay.cs
@@ -8,19 +8,38 @@ public class CardDisplay : MonoBehaviour {
     public CardValues card;
 
     public Text cardText;
-    public Text button1;
-    public Text button2;
-    public Text button3;
-    public Text button4;
+    public Text button1text;
+    public Text button2text;
+    public Text button3text;
+    public Text button4text;
+    public Text location;
+    public Button button1;
+    public Button button2;
+    public Button button3;
+    public Button button4;
+    public Sprite background;
+    public Sprite npc;
 
-    void Start () {
+    void Start()
+    {
 
-        cardText.text = card.cardText;
-        button1.text = card.option1;
-        button2.text = card.option2;
-        button3.text = card.option3;
-        button4.text = card.option4;
+    }
+    void Update () {
 
+        cardText.text = card.cardText.Replace("\\n", "\n");
+        button1text.text = card.option1;
+        button2text.text = card.option2;
+        button3text.text = card.option3;
+        button4text.text = card.option4;
+
+        Button option1 = button1.GetComponent<Button>();
+        option1.onClick.AddListener(Button1Pressed);
+
+    }
+    void Button1Pressed()
+    {
+        if(card.option1FollowCard)
+        card = card.option1FollowCard;
     }
 	
 }
