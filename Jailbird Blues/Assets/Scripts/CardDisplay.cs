b05bbb5d13eb6069bf6d15437c264889773c285e
Henri Leppänen <35915000+Zapperi@users.noreply.github.com>
2018-03-27 13:46:45 +0300

    Omaan branchiin, fadeImage coroutine, toimii. Vaihdettava float time.

--- Jailbird Blues/Assets/Scripts/CardDisplay.cs
+++ Jailbird Blues/Assets/Scripts/CardDisplay.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class CardDisplay : MonoBehaviour {
+public class CardDisplay : MonoBehaviour
+{
 
     public CardValues card;
 
@@ -19,18 +20,21 @@ public class CardDisplay : MonoBehaviour {
     public Button button4;
     public Sprite background;
     public Sprite npc;
+    public Image fadeImage;
+    public float fadeSpeed = 0.25f;
 
     void Start()
     {
-       
+        fadeImage.gameObject.SetActive(false);
 
 
     }
-    void Update () {
+    void Update()
+    {
 
         //Replace any \n in card's text string with line break
         cardText.text = card.cardText.Replace("\\n", "\n");
-        
+
         //Update options's texts to current ones
         button1text.text = card.option1;
         button2text.text = card.option2;
@@ -38,6 +42,10 @@ public class CardDisplay : MonoBehaviour {
         button4text.text = card.option4;
 
         //Update current button functions
+        button1.onClick.RemoveAllListeners();
+        button2.onClick.RemoveAllListeners();
+        button3.onClick.RemoveAllListeners();
+        button4.onClick.RemoveAllListeners();
         button1.onClick.AddListener(button1pressed);
         button2.onClick.AddListener(button2pressed);
         button3.onClick.AddListener(button3pressed);
@@ -98,10 +106,13 @@ public class CardDisplay : MonoBehaviour {
         //If card is not a result card, do this..
         if (card.option1FollowCard)
         {
+            
+            StartCoroutine(FadeImage(fadeSpeed));
             //Update the reputation values in gameController with current card values.
             GameController.gameController.UpdateReputations(card.option1IrsReputation, card.option1PunkReputation, card.option1ShakeReputation, card.option1GuardReputation);
             //Setup the result card.
             card = card.option1FollowCard;
+            
         }
         else { }
         //If card is a result card, do this..
@@ -111,7 +122,8 @@ public class CardDisplay : MonoBehaviour {
         //If card is not a result card, do this..
         if (card.option2FollowCard)
         {
-			GameController.gameController.UpdateReputations(card.option2IrsReputation, card.option2PunkReputation, card.option2ShakeReputation, card.option2GuardReputation);
+            StartCoroutine(FadeImage(fadeSpeed));
+            GameController.gameController.UpdateReputations(card.option2IrsReputation, card.option2PunkReputation, card.option2ShakeReputation, card.option2GuardReputation);
             card = card.option2FollowCard;
         }
         else { }
@@ -122,7 +134,8 @@ public class CardDisplay : MonoBehaviour {
         //If card is not a result card, do this..
         if (card.option3FollowCard)
         {
-			GameController.gameController.UpdateReputations(card.option3IrsReputation, card.option3PunkReputation, card.option3ShakeReputation, card.option3GuardReputation);
+            StartCoroutine(FadeImage(fadeSpeed));
+            GameController.gameController.UpdateReputations(card.option3IrsReputation, card.option3PunkReputation, card.option3ShakeReputation, card.option3GuardReputation);
             card = card.option3FollowCard;
         }
         else { }
@@ -130,13 +143,35 @@ public class CardDisplay : MonoBehaviour {
     }
     void button4pressed()
     {
+
         //If card is not a result card, do this..
         if (card.option4FollowCard)
         {
-			GameController.gameController.UpdateReputations(card.option4IrsReputation, card.option4PunkReputation, card.option4ShakeReputation, card.option4GuardReputation);
+            StartCoroutine(FadeImage(fadeSpeed));
+            GameController.gameController.UpdateReputations(card.option4IrsReputation, card.option4PunkReputation, card.option4ShakeReputation, card.option4GuardReputation);
             card = card.option4FollowCard;
         }
         else { }
         //If card is a result card, do this..
     }
+
+    //Coroutine for image fade between cards, takes in time (float) as parameter
+    IEnumerator FadeImage(float time)                                       
+    {
+        fadeImage.gameObject.SetActive(true);                           //Activate the overlaying image
+        // fade from transparent to opaque
+        for (float i = 0; i <= time; i += Time.deltaTime)               // loop over time second
+        {
+            fadeImage.color = new Color(0.25f, 0.25f, 0.25f, i);        // set color with i as alpha
+            yield return null;                                          // Continue coroutine
+        }
+        // fade from opaque to transparent
+        for (float i = time; i >= 0; i -= Time.deltaTime)               // loop over time second backwards
+        {
+                fadeImage.color = new Color(0.25f, 0.25f, 0.25f, i);    // set color with i as alpha
+            yield return null;                                          // Continue coroutine
+        }
+            fadeImage.gameObject.SetActive(false);                      //Deactivate the overlaying image
+    }
 }
+
