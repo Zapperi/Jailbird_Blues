4d4338c130440f1ce20ff66f0251f90dc3e49499
aarneh <aarneh@metropolia.fi>
2018-04-16 13:19:13 +0300

    Optionbutton required switch hover images

---
+++ Jailbird Blues/Assets/Scripts/ButtonHoverScripts/Button2Hover.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Button2Hover : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+
+    public GameObject cardDisplay;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        cardDisplay.GetComponent<CardDisplay>().button2Hover = true;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        cardDisplay.GetComponent<CardDisplay>().button2Hover = false;
+    }
+}
