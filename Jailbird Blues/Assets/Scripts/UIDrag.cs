01157df352c1ccf64bdb1994727f157e0d52eb20
Wyrmi <cyan.wyrmie@gmail.com>
2018-04-18 14:10:33 +0300

    Notebook drag fix to screenspace - camera

--- Jailbird Blues/Assets/Scripts/UIDrag.cs
+++ Jailbird Blues/Assets/Scripts/UIDrag.cs
@@ -3,23 +3,22 @@ using System.Collections;
 
 public class UIDrag : MonoBehaviour
 {
-	//copypastascripti youtube tutorialista https://www.youtube.com/watch?v=hzuxb8CPGyQ
+	//copypastascripti worldspace/screenspace - overlay canvasmoodeihin youtube tutorialista https://www.youtube.com/watch?v=hzuxb8CPGyQ
 	//mahdollistaa pelin aikana dragattavan UIn
 	//Liitä haluttuun UIhin tämä scripti ja Event Trigger componentti
 	//lisää Event Triggeriin alla olevat functiot inspectorista
 
-	private float offsetX;
-	private float offsetY;
+	Vector3 screenPoint;
 
 	//Event Trigger -> Begin Drag(base event data)
 	public void BeginDrag()
 	{
-		offsetX = transform.position.x - Input.mousePosition.x;
-		offsetY = transform.position.y - Input.mousePosition.y;
 	}
 	//Event Trigger -> Drag(base event data)
 	public void OnDrag()
 	{
-		transform.position = new Vector3(offsetX + Input.mousePosition.x, offsetY + Input.mousePosition.y);
+		screenPoint = Input.mousePosition;
+		screenPoint.z = 1f; //distance of the plane from the camera
+		transform.position = Camera.main.ScreenToWorldPoint(screenPoint);
 	}
 }﻿
