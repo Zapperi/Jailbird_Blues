3f7feba6c59e27800b508f4150543040dab0b6be
Dacapo <henrilep@metropolia.fi>
2018-04-19 23:04:15 +0300

    Added fade to the floating text

--- Jailbird Blues/Assets/Scripts/GainTextGeneration.cs
+++ Jailbird Blues/Assets/Scripts/GainTextGeneration.cs
@@ -11,12 +11,14 @@ public class GainTextGeneration : MonoBehaviour {
     void OnEnable () {                                                          // When this object is called..
         AnimatorClipInfo[] clipInfo = _animator.GetCurrentAnimatorClipInfo(0);  // Get information about the animators on this object (only one on this object).
         Destroy(gameObject, clipInfo[0].clip.length);                           // Destroy te object after the lenght of the text float animation.
+        _animator.GetComponent<CanvasRenderer>().SetAlpha(1f);
+        _animator.GetComponent<Text>().CrossFadeAlpha(0f, clipInfo[0].clip.length, false);
         _gainsText = _animator.GetComponent<Text>();                            // Set the text to correspond what shows in the animator.
 	}
 	
     public void SetText(string newText)                                            // Function that sets the text of the floating repuation gain text.
     {
-       _gainsText.text = newText;                                                  // Set the text.
+       
+        _gainsText.text = newText;                                                  // Set the text.
     }
-	
 }
